88f64ae1a1c9a65bafb4eaacaac301feab673c6f
Ismael Catalá Gil <icatala@me.com>
2022-09-23 09:25:39 +0200

    changes railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
   public function boot()
   {
     //
+      if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+    }
   }
 }
